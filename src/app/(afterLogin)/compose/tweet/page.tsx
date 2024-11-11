25dfa00adc77520874bec225e0cb32c05913331e
realyuki <realyuki@ncsoft.com>
2024-11-11 15:44:26 +0900

    chore: TweetModal 유지 처리

--- src/app/(afterLogin)/compose/tweet/page.tsx
+++ src/app/(afterLogin)/compose/tweet/page.tsx
@@ -1,5 +1,11 @@
+import TweetModal from '@/app/(afterLogin)/@modal/(.)compose/tweet/page'
 import Home from '@/app/(afterLogin)/home/page'
 
 export default function Page() {
-  return <Home />
+  return (
+    <>
+      <Home />
+      <TweetModal />
+    </>
+  )
 }
